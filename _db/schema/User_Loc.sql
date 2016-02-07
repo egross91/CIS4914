866d5f749f7a32f8292312e123c91b7c22140b2d
Eric <eric.bgross@gmail.com>
2016-02-06 20:28:16 -0500

    [DB] Added user location schema.

---
+++ _db/schema/User_Loc.sql
@@ -0,0 +1,7 @@
+CREATE TABLE User_Loc (
+	userId    INT NOT NULL,
+	longitude NUMERIC(7, 4), -- ABC.WXYZ
+	latitude  NUMERIC(7, 4), -- ABC.WXYZ
+	FOREIGN KEY (userId)
+		REFERENCES User_Pers(userId) ON DELETE CASCADE
+);
--- _db/scripts/CreateAllTables.sh
+++ _db/scripts/CreateAllTables.sh
@@ -1,3 +1,4 @@
 %MU_PSQL%
 \i ../schema/User_Pers.sql;
 \i ../schema/User_Gen.sql;
+\i ../schema/User_Loc.sql;
